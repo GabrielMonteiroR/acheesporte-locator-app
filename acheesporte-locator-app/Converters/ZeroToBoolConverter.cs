a9450292751d7fd44b7c648a295bb438a036a97b
Gabriel <gmonteiro810@gmail.com>
2025-06-14 15:45:44 -0300

    Add value converters for improved data binding
    Updated project structure to include a new folder for converters. Added multiple converter classes:
    - `BoolToSimNaoConverter` for translating boolean values to "Sim" and "Não".
    - `EmptyFieldToColorConverter` for returning colors based on string input.
    - `InverseBoolConverter` for inverting boolean values.
    - `ZeroToBoolConverter` for converting integers to boolean based on zero.
    - `ZeroToFalseConverter` for checking if integers are greater than zero.
    
    All converters implement the `IValueConverter` interface with appropriate methods.

---
+++ acheesporte-locator-app/Converters/ZeroToBoolConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace acheesporte_locator_app.Converters;
+
+public class ZeroToBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        (int)value == 0;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
---
+++ acheesporte-locator-app/Converters/ZeroToFalseConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace acheesporte_locator_app.Converters;
+
+public class ZeroToFalseConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is int i && i > 0;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
