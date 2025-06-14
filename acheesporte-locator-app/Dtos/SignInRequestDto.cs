ad10ebc16773c0f980e6f63e9f46ea9edfaf2131
Gabriel <gmonteiro810@gmail.com>
2025-06-14 11:51:04 -0300

    Add DTOs for sign-in requests and responses
    Introduce `SignInRequestDto` for user authentication with
    validated `Email` and `Password` properties. Add
    `SignInResponseDto` to encapsulate user details such as
    `Token`, `Id`, `FirstName`, `LastName`, `Email`, `Phone`,
    `RoleId`, `RoleName`, `IsBanned`, `CreatedAt`, and
    `UpdatedAt`, all with JSON serialization attributes.
    These changes improve the management of user authentication
    data in the application.

---
+++ acheesporte-locator-app/Dtos/SignInRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace acheesporte_locator_app.Dtos;
+
+public class SignInRequestDto
+{
+    [Required]
+    [JsonPropertyName("email")]
+    [EmailAddress(ErrorMessage = "Invalid email address format.")]
+    public string Email { get; set; };
+
+    [Required]
+    [JsonPropertyName("password")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; };
+}
