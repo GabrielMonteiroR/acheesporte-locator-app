10a722d8624fbf85e18e674bf062480dc3dfbcdb
Gabriel <gmonteiro810@gmail.com>
2025-06-14 15:28:45 -0300

    Enhance UserService with sign-in functionality
    - Added using directives for necessary namespaces.
    - Implemented a constructor to initialize HttpClient and ApiSettings.
    - Introduced SignInUserAsync method for user authentication.
    - Added error handling for login failures and exceptions.
    - Securely stores authentication token upon successful login.

--- acheesporte-locator-app/Services/UserService.cs
+++ acheesporte-locator-app/Services/UserService.cs
@@ -1,8 +1,44 @@
-﻿using acheesporte_locator_app.Interfaces;
+﻿using acheesporte_locator_app.Config;
+using acheesporte_locator_app.Dtos;
+using acheesporte_locator_app.Interfaces;
+using System.Net.Http.Json;
 
 namespace acheesporte_locator_app.Services;
 
 public class UserService : IUserInterface
 {
+    private readonly HttpClient _httpClient;
+    private readonly ApiSettings _apiSettings;
 
+    public UserService(HttpClient httpClient, ApiSettings apiSettings)
+    {
+        _httpClient = httpClient;
+        _apiSettings = apiSettings;
+    }
+
+    public async Task<SignInResponseDto> SignInUserAsync(SignInRequestDto request)
+    {
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync(_apiSettings.SignInEndpoint, request);
+            if (response.IsSuccessStatusCode)
+            {
+                var loginResponse = await response.Content.ReadFromJsonAsync<SignInResponseDto>();
+
+                if (loginResponse is null || string.IsNullOrEmpty(loginResponse.Token))
+                {
+                    throw new Exception("Login failed. Invalid response from server.");
+                }
+                await SecureStorage.SetAsync("auth_token", loginResponse.Token);
+                return loginResponse;
+            }
+
+            var backendError = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Login failed: {backendError}");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("An error occurred while logging in.", ex);
+        }
+    }
 }
