27c3fc7e41945bbc4a23074b3e5da8243f74a3ea
Gabriel <gmonteiro810@gmail.com>
2025-06-14 16:06:59 -0300

    Add user authentication flow with splash screen
    Updated MauiProgram.cs to include Login and Register pages,
    along with a new SplashPage for user authentication.
    Configured HTTP clients for image and user services.
    Modified project file to compile new SplashPage.xaml and
    added logic for user session management in SplashPage.xaml.cs.

--- acheesporte-locator-app/MauiProgram.cs
+++ acheesporte-locator-app/MauiProgram.cs
@@ -1,6 +1,9 @@
-﻿using acheesporte_locator_app.Config;
+﻿using acheesporte_athlete_app.Views;
+using acheesporte_locator_app.Config;
 using acheesporte_locator_app.Interfaces;
 using acheesporte_locator_app.Services;
+using acheesporte_locator_app.ViewModels;
+using acheesporte_locator_app.Views;
 using CommunityToolkit.Maui;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -25,6 +28,21 @@ public static class MauiProgram
         builder.Services.Configure<ApiSettings>(config.GetSection("ApiSettings"));
         builder.Services.AddSingleton(resolver =>
             resolver.GetRequiredService<IOptions<ApiSettings>>().Value);
+
+        builder.Services.AddTransient<LoginPage>();
+        builder.Services.AddTransient<LoginViewModel>();
+        builder.Services.AddTransient<RegisterPage>();
+        builder.Services.AddTransient<RegisterViewModel>();
+        builder.Services.AddSingleton<AppShell>();
+        builder.Services.AddTransient<SplashPage>();
+
+
+
+        builder.Services.AddHttpClient<IImageInterface, ImageService>(client =>
+        {
+            var apiSettings = builder.Configuration.GetSection("ApiSettings").Get<ApiSettings>();
+            client.BaseAddress = new Uri(apiSettings.BaseUrl);
+        });
 
         builder.Services.AddHttpClient<IUserInterface, UserService>(client =>
         {
