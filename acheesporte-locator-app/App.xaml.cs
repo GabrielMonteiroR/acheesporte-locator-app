6c1faf88878ad05952c40e0c9beb5b393cd09c9e
Gabriel <gmonteiro810@gmail.com>
2025-06-14 11:26:26 -0300

    Add project files.

---
+++ acheesporte-locator-app/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace acheesporte_locator_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ acheesporte-locator-app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace acheesporte_locator_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
