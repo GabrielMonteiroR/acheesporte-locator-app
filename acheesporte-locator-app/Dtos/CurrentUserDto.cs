91295dee52dd8b893da3603982f8a16df66e52c0
Gabriel <gmonteiro810@gmail.com>
2025-06-14 11:45:13 -0300

    Add CurrentUserDto and update project structure
    - Updated `acheesporte-locator-app.csproj` to include a new folder reference for "Services".
    - Introduced `CurrentUserDto` class with properties for user data and JSON serialization attributes.
    - Added a new interface `IUserInterface`, currently with an incomplete method declaration.

---
+++ acheesporte-locator-app/Dtos/CurrentUserDto.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace acheesporte_locator_app.Dtos;
+
+public class CurrentUserDto
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; }
+
+    [JsonPropertyName("roleId")]
+    public int RoleId { get; set; }
+
+    [JsonPropertyName("isBanned")]
+    public bool IsBanned { get; set; }
+
+    [JsonPropertyName("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("profileImage")]
+    public string? ProfileImage { get; set; }
+}
