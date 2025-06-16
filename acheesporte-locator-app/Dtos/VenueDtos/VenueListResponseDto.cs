bebe89017644b7f32dcf19a49f19b3421beb65ad
Gabriel <gmonteiro810@gmail.com>
2025-06-16 13:30:21 -0300

    Add VenueListResponseDto for venue data response
    Introduces the `VenueListResponseDto` class in the
    `acheesporte_locator_app.Dtos.VenueDtos` namespace.
    This class contains a `Message` string property and a
    `Data` property that holds a list of `VenueResponseDto`
    objects, with JSON property names specified for serialization.

---
+++ acheesporte-locator-app/Dtos/VenueDtos/VenueListResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace acheesporte_locator_app.Dtos.VenueDtos;
+
+public class VenueListResponseDto
+{
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+
+    [JsonPropertyName("data")]
+    public List<VenueResponseDto> Data { get; set; }
+}
