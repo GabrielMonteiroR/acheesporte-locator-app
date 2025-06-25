90fc5b3bf6b1d73d8d4503f1be581a2998c04a78
Gabriel <gmonteiro810@gmail.com>
2025-06-25 11:31:14 -0300

    Add UpdateUserRequestDto for user update operations
    Introduces a new class `UpdateUserRequestDto` in the
    `acheesporte_locator_app.Dtos.Users` namespace. This class
    serves as a data transfer object for updating user information
    with properties for `FirstName`, `LastName`, `Email`, and
    `Phone`, each annotated with `JsonPropertyName` for JSON
    serialization and deserialization.

---
+++ acheesporte-locator-app/Dtos/Users/UpdateUserRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace acheesporte_locator_app.Dtos.Users;
+
+public class UpdateUserRequestDto
+{
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; }
+}
