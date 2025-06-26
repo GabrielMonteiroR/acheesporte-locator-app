f6eb053712f1c5dc9b2f9f408107b87edd1f05a3
Gabriel <gmonteiro810@gmail.com>
2025-06-26 14:34:57 -0300

    Add ReservationsResponseDto class for reservation data
    Introduces the `ReservationsResponseDto` class in the
    `acheesporte_locator_app.Dtos.Reservations` namespace.
    This class contains two properties: `Message` (string)
    and `Reservations` (list of `ReservationResponseDto`),
    with JSON property names specified for serialization.

---
+++ acheesporte-locator-app/Dtos/Reservations/ReservationsResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace acheesporte_locator_app.Dtos.Reservations;
+
+public class ReservationsResponseDto
+{
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+
+    [JsonPropertyName("reservations")]
+    public List<ReservationResponseDto> Reservations { get; set; }
+}
